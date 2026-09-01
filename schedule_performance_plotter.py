0f09dadd0dd7bbc78321cd3128820631b272a1d2
austinmcdannald <austin.mcdannald@nist.gov>
2026-09-01 10:55:03 -0400

    Figure formattting

--- schedule_performance_plotter.py
+++ schedule_performance_plotter.py
@@ -144,6 +144,7 @@ fig.update_yaxes(title_text="Scheduled Time (das)", row=1, col=2)
 fig.update_yaxes(title_text="Speedup", row=1, col=3)
 
 fig.show()
+fig.write_image("schedule_scale_plot.svg",  width=1248, height=450)
 
 print("total compute, das", np.sum(das_compute_times))
 print("total compute, hs", np.sum(hs_compute_times))
